f2543bbd26d3e4270a66c3c8df45ca4bbe1311ca
Eric Gustavo Coronel Castillo <gcoronelc@gmail.com>
2017-12-09 11:53:11 -0500

    Clase 04

---
+++ Clase04/AprendiendoHerencia/src/pe/egcc/app/service/Clase2.java
@@ -0,0 +1,11 @@
+package pe.egcc.app.service;
+
+/**
+ *
+ * @author Gustavo Coronel
+ * @blog   gcoronelc.blogspot.com
+ * @email  gcoronelc@gmail.com
+ */
+public class Clase2  extends Clase1{
+
+}
